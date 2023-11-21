ad4dd12a193d741155317b8f383038879a388fee
王佳鑫 <wangjiaxin1@eastmoney.com>
2023-11-21 16:16:13 +0800

    feat: add react-learn page

---
+++ app/react-learn/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'ReactLearn',
+};
+
+export default async function Page() {
+  return (
+    <main>
+      react-learn
+    </main>
+  );
+}
